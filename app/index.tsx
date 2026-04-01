04c7f10a85bf7e4ef91d6674cc0f07f313cb1dea
AdemHos <ademhos1907@outlook.com>
2026-04-01 17:12:54 +0300

    First Commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import '@/global.css'
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-green-500"
+    >
+      <Text className="text-xl text-white">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
